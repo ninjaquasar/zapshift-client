bbfff8185e9d2d46aa2d72dde8ce92ffee06bc77
mahnaf <mahnaf125@gmail.com>
2025-06-25 20:17:53 +0600

    feat(auth): implement firebase auth logic

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,9 +1,53 @@
+import {
+	createUserWithEmailAndPassword,
+	GoogleAuthProvider,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signInWithPopup,
+	signOut,
+	updateProfile,
+} from "firebase/auth";
+import { auth } from "../../config/firebase.config";
+import { useEffect, useState } from "react";
+
 const useAuth = () => {
-	const staticUser = {
-		displayName: "ZapShift Dev",
-		email: "developer@zapshift.com",
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const registerUserWithEmailAndPassword = (email, password) => {
+		return createUserWithEmailAndPassword(auth, email, password);
 	};
-	return { staticUser };
+	const loginUserWithEmailAndPassword = (email, password) => {
+		return signInWithEmailAndPassword(auth, email, password);
+	};
+	const loginUserWithGoogle = () => {
+		const provider = new GoogleAuthProvider();
+		return signInWithPopup(auth, provider);
+	};
+	const updateUserProfile = (displayName, photoURL) => {
+		const updatedProfile = { displayName, photoURL };
+		return updateProfile(auth, updatedProfile);
+	};
+	const logoutUser = () => {
+		return signOut(auth);
+	};
+	useEffect(() => {
+		const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			setLoading(false);
+		});
+		return () => {
+			unSubscribe();
+		};
+	}, []);
+	return {
+		user,
+		loading,
+		registerUserWithEmailAndPassword,
+		loginUserWithEmailAndPassword,
+		loginUserWithGoogle,
+		updateUserProfile,
+		logoutUser,
+	};
 };
 
 export default useAuth;
